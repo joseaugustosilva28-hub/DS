d3f655c16f6d542140577f4d521adee0a6fd5f68
joseaugustosilva28-hub <jose.augusto.silva28@escola.pr.gov.br>
2026-07-27 23:19:16 -0300

    Update ex01_tupla.py

--- aula2/ex01_tupla.py
+++ aula2/ex01_tupla.py
@@ -1,1 +1,4 @@
-
+dias_uteis = ("Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira")
+print("Primeiro dia: ", dias_uteis[0])
+print("Último dia: ", dias_uteis[-1])
+print("Quantidade de dias: ", len(dias_uteis))
