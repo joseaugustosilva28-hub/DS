f4963b9936c52d3435edd0fe152292b6c61dda5e
joseaugustosilva28-hub <jose.augusto.silva28@escola.pr.gov.br>
2026-08-06 22:28:44 -0300

    Create ex4.py

---
+++ aula4/ex4.py
@@ -0,0 +1,26 @@
+class ContaBancaria:
+    def __init__(self, titular, saldo):
+        self.titular = titular
+        self.saldo = saldo
+
+    def depositar(self, valor):
+        self.saldo = self.saldo + valor
+
+    def sacar(self, valor):
+        if valor <= self.saldo:
+            self.saldo = self.saldo - valor
+        else:
+            print("Saldo insuficiente")
+
+    def extrato(self):
+        print("Titular:", self.titular)
+        print("Saldo:", self.saldo)
+
+
+conta = ContaBancaria("João", 500)
+
+conta.depositar(200)
+conta.sacar(100)
+conta.sacar(700)
+
+conta.extrato()
