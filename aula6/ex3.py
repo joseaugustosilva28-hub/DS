8086e05bca81ad6a1bbab2eeab92729d319f08f1
joseaugustosilva28-hub <jose.augusto.silva28@escola.pr.gov.br>
2026-08-06 22:32:18 -0300

    Create ex3.py

---
+++ aula6/ex3.py
@@ -0,0 +1,30 @@
+class Forma:
+    def area(self):
+        return 0
+
+
+class Triangulo(Forma):
+    def __init__(self, base, altura):
+        self.base = base
+        self.altura = altura
+
+    def area(self):
+        return (self.base * self.altura) / 2
+
+
+class Quadrado(Forma):
+    def __init__(self, lado):
+        self.lado = lado
+
+    def area(self):
+        return self.lado * self.lado
+
+
+formas = [
+    Triangulo(5, 4),
+    Quadrado(6),
+    Triangulo(3, 8)
+]
+
+for forma in formas:
+    print("Área:", forma.area())
